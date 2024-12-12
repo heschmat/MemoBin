bd577f21fd766496b7588fc4d034103adb30c955
Hesch <heschmat.seyedi@gmail.com>
2024-12-12 15:51:49 +0100

    Add basic view detail.

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome to MemoBin"))
 }
 
+
+func memoView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi((r.PathValue("id")))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific memo with ID %d...", id)
+	w.Write([]byte(msg))
+}
+
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home) // Restrict the route to exact matches on / only
+	mux.HandleFunc("/memo/view/{id}", memoView)
 
 	log.Print("Starting server on :4000")
 
