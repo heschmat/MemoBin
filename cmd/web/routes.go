fdc4f1543e849fd815537eba96041e36d5f6797d
Hesch <heschmat.seyedi@gmail.com>
2024-12-18 05:39:16 +0100

    Add composable middleware chains.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 
 func (app *application) routes() http.Handler {
@@ -17,5 +21,12 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("POST /memo/create", app.memoCreatePost)
 
 	// middlewares chain
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	// return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+
+	// Create a middleware chain containing our *standard* middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	// Return the standard middleware chain followed by the *servemux*.
+	return standard.Then(mux)
 }
